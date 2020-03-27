38e78d27e017de32edb071e4ae2062a3e0c216a2
titof <hktitof@gmail.com>
2020-03-27 23:28:59 +0100

    Change name of label

--- src/login_form/Lofin_Page.java
+++ src/login_form/Lofin_Page.java
@@ -60,7 +60,7 @@ public class Lofin_Page extends javax.swing.JFrame {
         jPasswordField1_Password.setText("jPassworim");
         getContentPane().add(jPasswordField1_Password, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 410, 250, 40));
 
-        jLabel1.setText("essakhi");
+        jLabel1.setText("Loloka");
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 90, -1, -1));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/login_form/last one 3.jpg"))); // NOI18N
